fdb193513eedd1dd1dd24a1ff4f1e8468fabde25
Patryk Kozlowski <pkozlows@caltech.edu>
2024-08-09 09:00:28 -0400

    Cate version

--- src/fns/h2plt.py
+++ src/fns/h2plt.py
@@ -27,16 +27,16 @@ for bond_distance in np.arange(0.5, 6.0, 0.25):
     fci_solver = fci.FCI(molecule, mf.mo_coeff)
     e, fci_wfn = fci_solver.kernel()
     fci_dm = fci_solver.make_rdm1(fci_wfn, norbs, mf.mol.nelectron)
-    # now do dtda
-    g0w0_dtda_dm = lin_gw_dm(my_dtda(mf), mf)
-    # assert that this density matrix is hermitian
-    assert np.allclose(g0w0_dtda_dm, g0w0_dtda_dm.T)
-    tda_e, tda_wfn = np.linalg.eigh(g0w0_dtda_dm)
-    # sort the energies and reverse order
-    tda_e = np.sort(tda_e)[::-1]
-    diag_tda = np.diag(tda_e)
+    # # now do dtda
+    # g0w0_dtda_dm = lin_gw_dm(my_dtda(mf), mf)
+    # # assert that this density matrix is hermitian
+    # assert np.allclose(g0w0_dtda_dm, g0w0_dtda_dm.T)
+    # tda_e, tda_wfn = np.linalg.eigh(g0w0_dtda_dm)
+    # # sort the energies and reverse order
+    # tda_e = np.sort(tda_e)[::-1]
+    # diag_tda = np.diag(tda_e)
     # now do drpa
-    g0w0_drpa_dm = lin_gw_dm(my_drpa(mf), mf)
+    g0w0_drpa_dm = lin_gw_dm(my_drpa, mf)
     # assert that this density matrix is hermitian
     assert np.allclose(g0w0_drpa_dm, g0w0_drpa_dm.T)
     drpa_e, drpa_wfn = np.linalg.eigh(g0w0_drpa_dm)
@@ -45,14 +45,14 @@ for bond_distance in np.arange(0.5, 6.0, 0.25):
     diag_drpa = np.diag(drpa_e)
 
     # Add occupations for each method
-    for key, dm in zip(['hf', 'fci', 'dtda', 'drpa'], [hf_dm, fci_dm, diag_tda, diag_drpa]):
+    for key, dm in zip(['hf', 'fci', 'drpa'], [hf_dm, fci_dm, diag_drpa]):
         occupations[key].append((dm[0, 0], dm[1, 1]))
 
 # Plotting
-markers = {'hf': '*', 'fci': '+', 'dtda': 'x', 'drpa': '^'}
+markers = {'hf': '*', 'fci': '+', 'drpa': '^'}
 colors = {1: 'b', 2: 'r'}
 for state in [1, 2]:
-    for method in ['hf', 'fci', 'dtda', 'drpa']:
+    for method in ['hf', 'fci', 'drpa']:
         plt.plot(bond_distances, [occ[state-1] for occ in occupations[method]], 
                  marker=markers[method], color=colors[state], linestyle='None', label=f"{method.upper()} State {state}" if state == 1 else None)
 
@@ -63,12 +63,12 @@ plt.ylabel('Natural Occupation')
 # Creating a custom legend
 from matplotlib.lines import Line2D
 legend_elements = [
-    Line2D([0], [0], color='b', lw=4, label='State 1 (Blue)'),
-    Line2D([0], [0], color='r', lw=4, label='State 2 (Red)'),
-    Line2D([0], [0], marker='*', color='w', label='HF', markerfacecolor='k', markeredgecolor='k', markersize=10),
-    Line2D([0], [0], marker='+', color='w', label='FCI', markerfacecolor='k', markeredgecolor='k', markersize=10),
-    Line2D([0], [0], marker='x', color='w', label='dTDA', markerfacecolor='k', markeredgecolor='k', markersize=10),
-    Line2D([0], [0], marker='^', color='w', label='dRPA', markerfacecolor='k', markeredgecolor='k', markersize=10),
+    Line2D([0], [0], color='b', lw=4, label='HOMO'),
+    Line2D([0], [0], color='r', lw=4, label='LUMO'),
+    Line2D([0], [0], marker='*', color='w', label='Hartree-Fock', markerfacecolor='k', markeredgecolor='k', markersize=10),
+    Line2D([0], [0], marker='+', color='w', label='Exact (Full CI)', markerfacecolor='k', markeredgecolor='k', markersize=10),
+    # Line2D([0], [0], marker='x', color='w', label='dTDA', markerfacecolor='k', markeredgecolor='k', markersize=10),
+    Line2D([0], [0], marker='^', color='w', label=rf'$\gamma^{{GW}}$', markerfacecolor='k', markeredgecolor='k', markersize=10),
 ]
 
 # Now add this updated legend to your plot
--- src/fns/total_energy_functionals.py
+++ src/fns/total_energy_functionals.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import pyscf
 from pyscf import tddft
 import pyscf.cc
@@ -8,8 +9,6 @@ import pyscf.gw.rpa
 from src.fns.mf import setup_molecule, calculate_mean_field
 from src.fns.dm import lin_gw_dm
 from src.fns.tda import my_dtda, my_drpa, symm_drpa
-from src.fns.pyscf_tddft_fn import pyscf_td
-
 
 class Correlation:
     def __init__(self, name, noninteracting, interacting):
@@ -40,12 +39,22 @@ class Correlation:
         omega_rpa = my_drpa(self.noninteracting)[0]
         return 0.5 * np.sum(omega_rpa - omega_tda)
 
-
 def total_energy(noninteracting, interacting, correlation_name, mf=False):
     if correlation_name == 'klein_interacting':
-        hcore = noninteracting.mol.intor_symmetric('int1e_kin') + interacting.mol.intor_symmetric('int1e_nuc')
+
+        kinetic_integrals_ao = noninteracting.mol.intor('int1e_kin')
+        # rotate the kinetic integrals to the MO basis using the noninteracting MO coefficients
+        kinetic_integrals_mo = np.einsum('ij,ik,jl->kl', kinetic_integrals_ao, noninteracting.mo_coeff, noninteracting.mo_coeff)
+        # rotate back to ao basis using the interacting MO coefficients
+        final_kinetic_integrals = np.einsum('ij,ai,bj->ab', kinetic_integrals_mo, noninteracting.mo_coeff, noninteracting.mo_coeff)
+
+        nuclear_integrals_ao = noninteracting.mol.intor('int1e_nuc')
+        nuclear_integrals_mo = np.einsum('ij,ik,jl->kl', nuclear_integrals_ao, noninteracting.mo_coeff, noninteracting.mo_coeff)
+        final_nuclear_integrals = np.einsum('ij,ai,bj->ab', nuclear_integrals_mo, interacting.mo_coeff, interacting.mo_coeff)
+        # we want hcore in ao basis for later use wwithin energy_elec
+        hcore = final_kinetic_integrals + final_nuclear_integrals
     else:
-        hcore = interacting.get_hcore()
+        hcore = noninteracting.get_hcore()
 
     interacting_hf = interacting.to_rhf()
     vhf = interacting_hf.get_veff()
@@ -59,16 +68,13 @@ def total_energy(noninteracting, interacting, correlation_name, mf=False):
     else:
         e_tot = e_elec + noninteracting.energy_nuc() + e_corr
         return e_tot, e_corr 
-    
 
-
-    
-
-
-# Define the input variables that we will iterate over
+# Define the input variables that we will interacting over
 species = ['water', 'nh3', 'lih']
 methods = ['hf']
-correlation_funcs = ['gm']
+correlation_funcs = ['gm', 'klein_interacting', 'klein_noninteracting']
+
+data = []
 
 for molecule_name in species:
     for method in methods:
@@ -84,28 +90,39 @@ for molecule_name in species:
         interacting_mf.mo_coeff = np.einsum('ij,jk->ik', noninteracting_mf.mo_coeff, my_gw_mo)
         interacting_mf.mo_occ = my_gw_occs
 
-        # Make a dictionary to tabulate the GW energy for different correlation functionals
-        gw_energy_dict = {}
         for correlation_name in correlation_funcs:
             mf_energy = total_energy(noninteracting_mf, noninteracting_mf, correlation_name, mf=True)
             gw_total_energy, gw_correlation_energy = total_energy(noninteracting_mf, interacting_mf, correlation_name)
-            gw_energy_dict[correlation_name] = gw_total_energy
-            print(f'hf energy for {molecule_name} with {correlation_name}: {mf_energy}')
+            print(f'HF energy for {molecule_name} with {correlation_name}: {mf_energy}')
             print(f'GW energy for {molecule_name} with {correlation_name}: {gw_total_energy}')
-            # print(f'GW correlation energy for {molecule_name} with {correlation_name}: {gw_correlation_energy}')
-            # define the total and correlation energy from the pyscf rpa function
-            # rpa = pyscf.gw.rpa.RPA(noninteracting_mf)
-            # rpa.kernel()
-            # tot_e = rpa.e_tot
-            # cor_e = rpa.e_corr
-            # print(f'Pyscf RPA total energy for {molecule_name} with {method}: {tot_e}')
-            # print(f'Pyscf RPA correlation energy for {molecule_name} with {method}: {cor_e}')
-            # get the ccsd(t) energy as a reference
+            
+            # Get the CCSD(T) energy as a reference
             noninteracting_as_hf = noninteracting_mf.to_rhf()
             mycc = pyscf.cc.CCSD(noninteracting_as_hf).run(verbose=0)
             et = mycc.ccsd_t()
             ccsd_t_energy = mycc.e_tot + et
             print(f'CCSD(T) total energy for {molecule_name} with {method}: {ccsd_t_energy}')
-            
-        
 
+            # Append results to the data list
+            data.append({
+                'Molecule': molecule_name,
+                'HF Energy': mf_energy,
+                'GW Energy': gw_total_energy,
+                'CCSD(T) Energy': ccsd_t_energy,
+                'Correlation Functional': correlation_name
+            })
+
+# Convert to a DataFrame and format molecule names
+df = pd.DataFrame(data)
+df['Molecule'] = df['Molecule'].replace({'water': 'H$_2$O', 'nh3': 'NH$_3$', 'lih': 'LiH'})
+
+# Create separate tables for each correlation functional
+for correlation_name in correlation_funcs:
+    df_subset = df[df['Correlation Functional'] == correlation_name].drop(columns=['Correlation Functional'])
+    latex_table = df_subset.to_latex(index=False, escape=False, float_format="%.6f")
+    table_filename = f"energy_table_{correlation_name}.tex"
+    
+    with open(table_filename, "w") as f:
+        f.write(latex_table)
+    
+    print(latex_table)
